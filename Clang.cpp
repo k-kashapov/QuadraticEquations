ae10221b14844c3e58fb63c0e3dceed51bee52a5
k-kashapov <52855633+k-kashapov@users.noreply.github.com>
2021-08-18 22:17:59 +0300

    Add files via upload

--- Clang.cpp
+++ Clang.cpp
@@ -1,118 +1,58 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <cmath>
+#include <math.h>
 
-#define STR_LEN 1000
-#define isletter(c) (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
-#define approved(c) (c >= '0' && c <= '9' || isletter(c) || c == '-' || c == '+')
+#pragma warning(disable:4996)
 
 int main(int argc, char* argv[])
 {
-	char var_name = 'x';
-
-	char c = getchar();
-	while (!approved(c)) // Пропускаем все символы, которые не явл. цифрами или буквами или + или -
-		c = getchar();
+	long double coefs[3] = { 0.0, 0.0, 0.0 }; // Коэф-ы в конечном уравнении
 	
-	char str[STR_LEN]; // Строка для запоминания цифр коэффициентов
-	char* ptr = str; 
-	long double last_coef = 0.0; // Последний введённый коэф.
-	int coef_len = 0; // Его длина
-	
-	long double a = 0.0, b = 0.0, cc = 0.0; // Коэф-ы в конечном уравнении
-	int other_side = 1; // Ввод чисел слева (1) или справа (-1) от равно
-
-	while (c != '\n') {
-		if (isspace(c));
-		else if (c == '-' || c == '+' || c == '=') {
-			
-			if (coef_len > 0) // Если ситуация по типу ...1234.0 +...
-			{
-				*ptr = '\0';
-				last_coef = other_side * (*(ptr - 1) == '-') ? -1 : (coef_len == 0) ? 1 : atof(ptr - coef_len);
-				last_coef *= other_side;
-				cc += last_coef;
-				coef_len = 0;
-			}
-			if (c == '=') other_side = -1;
-			else
-			{
-				*ptr++ = c; // Сохраняем знак следующего коэф-а
-				coef_len++;
-			}
-		}
-		else if (isdigit(c) || c == '.') 
-		{
-			*ptr++ = c;
-			coef_len++;
-		}
-		else
-		{
-			*ptr = '\0';
-			last_coef = (*(ptr - 1) == '-') ? -1 : (coef_len == 0) ? 1 : atof(ptr - coef_len);
-			last_coef *= other_side;
-			coef_len = 0;
-
-			if (isletter(c)) { // Если попалась буква, то запоминаем её как имя переменной в уравнении
-				var_name = c;
-				char next = getchar();
-
-				if (next == '^') {
-					char power = getchar();
-					switch (power)
-					{
-					case '2':
-						a += last_coef;
-						break;
-					case '1':
-						b += last_coef;
-						break;
-					case '0':
-						cc += last_coef;
-						break;
-					default:
-						printf("invalid power");
-						return 1;
-					}
-				}
-				else {
-
-					b += last_coef; // Была найдена буква, но не степень => это первая степень
-					if (next == '-') {
-						*ptr++ = '-';
-					}
-					
-				}
-			}
-			
-		}
-
-		c = getchar();
+	for (int i = 0; i < 3; i++) {
+		printf("Input coefficient of x^%d\n", 2 - i); 
+		scanf("%lf", &coefs[i]); // Считываем коэф-ы
 	}
 
-	if (coef_len > 0) // Чекаем, если последним был введен свободный член
-	{
-		*ptr = '\0';
-		last_coef = (*(ptr - 1) == '-') ? -1 : (coef_len == 0) ? 1 : atof(ptr - coef_len);
-		last_coef *= other_side;
-		cc += last_coef;
-		coef_len = 0;
-	}
+	long double d = coefs[1] * coefs[1] - 4 * coefs[0] * coefs[2]; // Дискриминант
 
-	printf("%f%c^2 %f%c %f = 0\n", a, var_name, b, var_name, cc);
+	printf("D = %lf\n", d);
 
-	long double d = b * b - 4 * a * cc;
-	printf("d = %lf\n", d);
-	if (d < 0) {
+	if (d < 0) { // Нет корней
 		printf("No roots");
 		return 0;
 	}
 	
-	long double x1, x2;
-	
-	x1 = (-b + sqrt(d)) / 2 / a;
-	x2 = (-b - sqrt(d)) / 2 / a;
+	if (coefs[0] == 0) { // Если а == 0
+		if (coefs[1]) // Если b != 0, то корень = -c / b
+		{
+			printf("One root: x = %lf", -coefs[2] / coefs[1]);
+			return 0;
+		}
+		else if (coefs[2] == 0)
+		{
+			printf("Any x will do"); // Все коэф-ы равны нулю => Х принадлежит (-Inf; Inf)
+			return 0;
+		}
+		else
+		{
+			printf("No roots");
+			return 0;
+		}
+	}
 
-	printf("Roots: %c1 = %lf, %c2 = %lf", var_name, x1, var_name, x2);
+	if (d == 0) {
+		long double x1 = -coefs[1] / 2 / coefs[0];
+		printf("One root: x = %lf", x1);
+		return 0;
+	}
+	else 
+	{
+		long double x1, x2;
+
+		x1 = (-coefs[1] + sqrt(d)) / 2 / coefs[0];
+		x2 = (-coefs[1] - sqrt(d)) / 2 / coefs[0];
+
+		printf("Roots: x1 = %lf, x2 = %lf", x1, x2);
+		return 0;
+	}
 }
