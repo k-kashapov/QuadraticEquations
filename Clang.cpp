d2010f9afae848427e20392177b05262ef7f9dc5
k-kashapov <52855633+k-kashapov@users.noreply.github.com>
2021-08-18 23:13:19 +0300

    Add files via upload

--- Clang.cpp
+++ Clang.cpp
@@ -5,41 +5,64 @@
 #pragma warning(disable:4996)
 
 
-int check_linear(long double a, long double b, long double c) {
-	if (a == 0) { // Если а == 0
-		if (b) // Если b != 0, то корень = -c / b
+int check_linear(long double a, long double b, long double c) 
+{
+	if (a == 0) 
+	{ // Если а == 0
+		if (b) 
 		{
-			printf("One root: x = %lf", -c / b);
 			return 1;
 		}
 		else if (c == 0)
 		{
-			printf("Any x will do"); // Все коэф-ы равны нулю => Х принадлежит (-Inf; Inf)
-			return 1;
+			// Все коэф-ы равны нулю => Х принадлежит (-Inf; Inf)
+			return 100;
 		}
 		else
 		{
-			printf("No roots");
-			return 1;
+			return -1;
 		}
 	}
+	
+	return 0;
 }
+/*Возвращает количество корней*/
+int solve(long double a, long double b, long double c, long double *x1, long double *x2) 
+{
+	
+	long double d = b * b - 4 * a * c; // Дискриминант
+	int lin = 0;
 
-void solve_quadratic(long double a, long double b, long double c, long double d) {
-	if (d == 0) {
-		long double x1 = -b / 2 / a;
-		printf("One root: x = %lf", x1);
-		return;
+	if (d < 0) 
+	{ // Нет корней
+		return 0;
+	}
+
+	if (lin = check_linear(a, b, c)) 
+	{
+		switch (lin)
+		{
+		case 1:
+			*x1 = -c / b;
+			return 1;
+		case 100:
+			return 100;
+		default:
+			return 0;
+		}
+		
+	}
+	else if(d == 0)
+	{
+		*x1 = -b / 2 / a;
+		return 1;
 	}
 	else
 	{
-		long double x1, x2;
-
-		x1 = (-b + sqrt(d)) / 2 / a;
-		x2 = (-b - sqrt(d)) / 2 / a;
-
-		printf("Roots: x1 = %lf, x2 = %lf", x1, x2);
-		return;
+		printf("D = %lf\n", d);
+		*x1 = (-b + sqrt(d)) / 2 / a;
+		*x2 = (-b - sqrt(d)) / 2 / a;
+		return 2;
 	}
 }
 
@@ -51,17 +74,27 @@ int main(int argc, char* argv[])
 	printf("Input coefficients: a b c\n");
 	scanf("%lf %lf %lf", &a, &b, &c); // Считываем коэф-ы
 
-	long double d = b * b - 4 * a * c; // Дискриминант
-
-	printf("D = %lf\n", d);
-
-	if (d < 0) { // Нет корней
-		printf("No roots");
-		return 0;
-	}
+	long double x1 = 0.0, x2 = 0.0;
 	
-	if (!check_linear(a, b, c))
-		solve_quadratic(a, b, c, d);
+	char roots = solve(a, b, c, &x1, &x2);
+
+	switch (roots) // Смотрим, сколько корней
+	{
+	case 0:
+		printf("No roots");
+		break;
+	case 1:
+		printf("One root: x = %lf", x1);
+		break;
+	case 2:
+		printf("Roots: x1 = %lf, x2 = %lf", x1, x2);
+		break;
+	case 100:
+		printf("Any x will do");
+		break;
+	default:
+		break;
+	}
 	
 	return 0;
 }
