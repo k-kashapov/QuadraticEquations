2e16f6ebea1f52cbea150f164c9aa73b4d8af8ad
k-kashapov <52855633+k-kashapov@users.noreply.github.com>
2021-08-19 15:06:29 +0300

    Add files via upload

--- Clang.cpp
+++ Clang.cpp
@@ -1,81 +1,21 @@
 ﻿#include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
+#include <assert.h>
 
 #pragma warning(disable:4996)
 
 #define INF_ROOTS 100
 #define EPSILON 0.000001
 
-/*Проверяет, решается ли уравнение как линейное*/
-int check_linear(long double a, long double b, long double c) 
-{
-	if (a == 0) 
-	{ // Если а == 0
-		if (b) 
-		{
-			return 1;
-		}
-		else if (c == 0)
-		{
-			// Все коэф-ы равны нулю => Х принадлежит (-Inf; Inf)
-			return INF_ROOTS;
-		}
-		else
-		{
-			return -1;
-		}
-	}
-	
-	return 0;
-}
+/*Принимает коэф-ы b и c, указатель на x1. Решает уравнение как линейное, возвращает количество корней: 1, INF или 0, ответ помещает в *x1 */
+int solve_linear(long double b, long double c, long double* x1);
 
-char is_equal_approx(long double a, long double b) 
-{
-	return (abs(a - b) <= EPSILON);
-}
+/*Сравнивает два числа типа long double*/
+char is_equal_approx(long double a, long double b);
 
-/*Возвращает количество корней*/
-int solve(long double a, long double b, long double c, long double *x1, long double *x2) 
-{
-	
-	long double d = b * b - 4 * a * c; // Дискриминант
-	int lin = 0;
+/*Принимает коэффициенты a, b и c, указатели на x1 и x2. Возвращает количество корней, результаты вычислений помещает в *x1 и *x2 */
+int solve(long double a, long double b, long double c, long double* x1, long double* x2);
 
-	if (d < 0) 
-	{ // Нет корней
-		return 0;
-	}
-
-	long double d_sqrt = sqrt(d);
-
-	if (lin = check_linear(a, b, c)) 
-	{
-		switch (lin)
-		{
-		case 1:
-			*x1 = -c / b;
-			return 1;
-		case INF_ROOTS:
-			return INF_ROOTS;
-		default:
-			return 0;
-		}
-		
-	}
-	else if(is_equal_approx(d, 0))
-	{
-		*x1 = -b / 2 / a;
-		return 1;
-	}
-	else
-	{
-		printf("D = %lf\n", d);
-		*x1 = (-b + d_sqrt) / 2 / a;
-		*x2 = (-b - d_sqrt) / 2 / a;
-		return 2;
-	}
-}
 
 int main(int argc, char* argv[])
 {
@@ -100,7 +40,7 @@ int main(int argc, char* argv[])
 	case 2:
 		printf("Roots: x1 = %lf, x2 = %lf", x1, x2);
 		break;
-	case 100:
+	case INF_ROOTS:
 		printf("Any x will do");
 		break;
 	default:
@@ -109,4 +49,66 @@ int main(int argc, char* argv[])
 	}
 	
 	return 0;
+}
+
+char is_equal_approx(long double a, long double b)
+{
+	return (abs(a - b) <= EPSILON);
+}
+
+int solve_linear(long double b, long double c, long double* x1)
+{
+	if (!is_equal_approx(b, 0))
+	{
+		*x1 = -c / b;
+		return 1;
+	}
+	else if (is_equal_approx(c, 0))
+	{
+		// Все коэф-ы равны нулю => Х принадлежит (-Inf; Inf)
+		return INF_ROOTS;
+	}
+	else
+	{
+		return 0;
+	}
+
+
+	return 0;
+}
+
+int solve(long double a, long double b, long double c, long double* x1, long double* x2)
+{
+	assert(isfinite(a));
+	assert(isfinite(b));
+	assert(isfinite(c));
+	assert(x1 != NULL);
+	assert(x2 != NULL);
+
+	long double d = b * b - 4 * a * c; // Дискриминант
+	int lin = 0;
+
+	if (d < 0)
+	{ // Нет корней
+		return 0;
+	}
+
+	long double d_sqrt = sqrt(d);
+
+	if (a == 0)
+	{
+		return solve_linear(b, c, x1);
+	}
+	else if (is_equal_approx(d, 0))
+	{
+		*x1 = -b / 2 / a;
+		return 1;
+	}
+	else
+	{
+		printf("D = %lf\n", d);
+		*x1 = (-b + d_sqrt) / 2 / a;
+		*x2 = (-b - d_sqrt) / 2 / a;
+		return 2;
+	}
 }
